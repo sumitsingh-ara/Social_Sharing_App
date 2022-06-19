f391ffa323673d57f1afcbff5ff996e44d0100e3
Sumit Singh <sumit47919@gmail.com>
2022-06-20 02:20:23 +0530

    Comments integrated

--- frontEnd/src/components/Todos/Comments.jsx
+++ frontEnd/src/components/Todos/Comments.jsx
@@ -2,7 +2,8 @@ import "./Todo.css";
 import {Link} from "react-router-dom";
 import {useSelector} from "react-redux";
 export const Comments = ({comment}) => {
-    const {userName} = useSelector((store) => store.users)
+    const {userName} = useSelector((store) => store.users);
+    const url="https://media1.giphy.com/media/Oj5w7lOaR5ieNpuBhn/giphy.gif?cid=ecf05e47tnn5gp3m9bsqxf6zdnubk6e51c2o50ao8vpryyhz&rid=giphy.gif&ct=g"
   return (
     <>
       <div className="container mt-1">
@@ -13,9 +14,10 @@ export const Comments = ({comment}) => {
                 <div className="user d-flex flex-row align-items-center">
                   <img
                     alt="images"
-                    src="https://i.imgur.com/hczKIze.jpg"
-                    width="30"
-                    className="user-img rounded-circle mr-2"
+                    src={url}
+                    width="38"
+                   
+                    className="img-fluid img-responsive rounded-circle mr-2 "
                   />
                   <span>
                     <small className="font-weight-bold text-primary">
@@ -38,12 +40,11 @@ export const Comments = ({comment}) => {
                   <span className="dots"></span>
                   <small>Reply</small>
                   <span className="dots"></span>
-                  <small>Translate</small>
+                  {userName===comment.user.username?<small>Edit</small>:""}
                 </div>
-
                 <div className="icons align-items-center">
                   <i className="fa fa-star text-warning"></i>
-                  <i className="fa fa-check-circle-o check-icon"></i>
+                  <i class="fa fa-light fa-thumbs-up text-secondary hover-on-like" ></i>
                 </div>
               </div>
             </div>
